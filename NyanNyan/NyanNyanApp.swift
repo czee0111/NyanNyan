160b35c3df6875023dd57573a0878873af06db04
BT_C09_27 <mobileappdev@bths.edu>
2026-05-21 09:23:18 -0400

    Initial Commit

---
+++ NyanNyan/NyanNyanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NyanNyanApp.swift
+//  NyanNyan
+//
+//  Created by Student on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct NyanNyanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
